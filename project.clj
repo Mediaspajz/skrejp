bca51c170e94c4de70885c0898a5a60295824bf5
infiniteiteration <infiniteiteration@users.noreply.github.com>
2014-10-14 11:09:17 +0200

    reorganized deps, added marginalia

--- project.clj
+++ project.clj
@@ -3,16 +3,19 @@
   :url           "http://example.com/FIXME"
   :license       {:name "Eclipse Public License"
                   :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :plugins      [[lein-expectations "0.0.7"]]
+  :plugins      [[lein-expectations "0.0.7"]
+                 [lein-marginalia   "0.8.0"]]
   :core.typed    {:check [clj-scrapers.scraper]}
-  :dependencies [[org.clojure/clojure     "1.6.0"]
+  :dependencies [[org.clojure/clojure     "1.7.0-alpha2"]
                  [org.clojure/core.async  "0.1.319.0-6b1aca-alpha"]
                  [prismatic/schema        "0.2.6"]
+                 [com.stuartsierra/component "0.2.2"]
+
                  [http-kit                "2.1.16"]
                  [clojurewerkz/urly       "1.0.0"]
                  [enlive                  "1.1.5"]
-                 [com.gfredericks/vcr-clj "0.3.3"]
                  [expectations            "2.0.6"]
+
                  [clojurewerkz/elastisch  "2.0.0"]
                  [org.clojars.scsibug/feedparser-clj "0.4.0"]]
   :main          clj-scrapers.archives
--- src/clj_scrapers/archives.clj
+++ src/clj_scrapers/archives.clj
@@ -8,56 +8,34 @@
   (:require [net.cgrand.enlive-html :as html])
   )
 
-(defn- gen-scrape-index
-  [article-href-sel]
-  (fn [url]
-    (let
-      [ article-pages-c (chan 64)
-        page-result-c
-          (fetch-page url
-                      (fn [body]
-                        (map #(get-in % [:attrs :href])
-                             (extract-sel body article-href-sel))
-                        )) ]
-      (go (onto-chan article-pages-c
-                     (map (partial urly/resolve url)
-                          (<! page-result-c))))
-      article-pages-c))
+(defn scrape-index-page [url article-urls-c]
+  (let
+      [ page-result-c
+        (fetch-page url
+                    (fn [body]
+                      (map #(get-in % [:attrs :href])
+                           (extract-sel body [:div#content :span.field-content :a]))
+                      )) ]
+    (go (onto-chan article-urls-c
+                   (map (partial urly/resolve url)
+                        (<! page-result-c)))
+        (async/close! article-urls-c)
+        )))
+
+(defn walk-index-pages [initial-url index-page-urls-c]
+  (go
+    (>! index-page-urls-c initial-url)
+    (loop [current-url initial-url]
+      (let [next-url-c
+            (fetch-page current-url
+                        #(extract-href % [:div#content :ul.pager :li.pager-next :a]))
+            next-url (urly/resolve initial-url (<! next-url-c)) ]
+        (>! index-page-c next-url)
+        (recur next-url)
+        ))
+    (async/close! index-page-urls-c)
+    )
   )
-
-(defn- gen-scrape-next-indexes
-  [next-href-sel]
-  (fn [initial-url]
-    (let [ index-page-c (chan 100) ]
-      (go
-        (>! index-page-c initial-url)
-        (loop [current-url initial-url]
-          (let [next-url-c
-                (fetch-page current-url
-                            (fn [body] (extract-href body next-href-sel)))
-                next-url (urly/resolve initial-url (<! next-url-c)) ]
-            (>! index-page-c next-url)
-            (recur next-url)
-            )))
-      index-page-c
-      )))
-
-(defmulti scrape-index        classify-url-source)
-(defmulti scrape-next-indexes classify-url-source)
-
-(defmacro defindexscraper
-  "Define an index scraper for a source."
-  [source index-link-sel next-index-link-sel]
-  `(let [ source-kw#           ~(source-keyword (name source))
-          scrape-index#        (gen-scrape-index        ~index-link-sel)
-          scrape-next-indexes# (gen-scrape-next-indexes ~next-index-link-sel) ]
-       (defmethod scrape-index        source-kw# [url#] (apply scrape-index# [url#]))
-       (defmethod scrape-next-indexes source-kw# [url#] (apply scrape-next-indexes# [url#]))
-     ))
-
-(defindexscraper "ujszo.com"
-                 [:div#content :span.field-content :a]
-                 [:div#content :ul.pager :li.pager-next :a])
 
 (defn sink [f c]
   (go (loop []
