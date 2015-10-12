9cad716bb2143f9e06c20038d2a526f2b27f294b
infiniteiteration <infiniteiteration@users.noreply.github.com>
2015-10-12 16:13:43 +0200

    dep update

--- project.clj
+++ project.clj
@@ -18,7 +18,7 @@
                  [org.clojure/core.typed "0.3.11"]
                  [org.clojure/core.async "0.1.319.0-6b1aca-alpha"]
                  [com.stuartsierra/component "0.3.0"]
-                 [org.clojure/tools.reader "0.10.0-alpha1"]
+                 [org.clojure/tools.reader "0.10.0"]
 
                  ;; crawling, parsing, scraping
                  [org.clojars.scsibug/feedparser-clj "0.4.0"]
